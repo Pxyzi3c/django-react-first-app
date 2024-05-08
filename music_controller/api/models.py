fb3f711ccff6f7d46ce5e9107405d5c4082591cd
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-05-08 14:49:37 +0800

    Implement handling of POST Requests

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import string
 import random
 
-def geenrate_unique_code():
+def generate_unique_code():
     length = 6
 
     while True:
@@ -14,8 +14,11 @@ def geenrate_unique_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)        
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -3,22 +3,21 @@ import {
     BrowserRouter as Router, 
     Route,
     Routes, 
-    Link, 
     Redirect 
 } from "react-router-dom";
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
-
+ 
 export default function HomePage() {
     return (
-        <>
+        <>  
             <Router>
                 <Routes>
-                    <Route index element={<h1>This is the homepage</h1>} />
+                    <Route index element={<h1>Welcome to the league of Harvy!</h1>} />
                     <Route path="join" element={<RoomJoinPage />} />
                     <Route path="create" element={<CreateRoomPage />} />
                 </Routes>
             </Router>
-        </>
+        </> 
     )
 }
