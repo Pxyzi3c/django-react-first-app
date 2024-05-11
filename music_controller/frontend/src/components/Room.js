21191db67c7ad1fe6a4ba1daff3ae7f6e70d4a2a
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-05-11 20:04:48 +0800

    create get endpoints

---
+++ music_controller/frontend/src/components/Room.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+export default function Room() {
+    const [votesToSkip, setVotesToSkip] = useState(2);
+    const [guestCanPause, setGuestCanPause] = useState(false);
+    const [isHost, setIsHost] = useState(false);
+
+    const { roomCode } = useParams();
+
+    const handleGetRoom = async () => {
+        try {
+            const response = await axios.get("/api/get-room" + "?code=" + roomCode);
+            
+            setVotesToSkip(response.data.votes_to_skip)
+            setGuestCanPause(response.data.guest_can_pause)
+            setIsHost(response.data.is_host)
+            console.log(response.data);
+        } catch (error) {
+            console.log("Error fetching room:", error);
+        }
+    }
+
+    useEffect(() => {
+        handleGetRoom();
+    }, [roomCode]);
+
+    return (
+        <div class="container">
+            <h3>Room Code: {roomCode}</h3>
+            <p>Votes: {votesToSkip}</p>
+            <p>Guest Can Pause: {guestCanPause.toString()}</p>
+            <p>Host: {isHost.toString()}</p>
+        </div>
+    )
+}
