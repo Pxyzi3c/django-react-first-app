9cb107b2b802bb817dcba415954116a8d1852e8f
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-06-29 10:07:52 +0800

    Implement pause/play functionality

--- music_controller/spotify/urls.py
+++ music_controller/spotify/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('get-auth-url', AuthAURL.as_view()),
     path('redirect', SpotifyCallbackView.as_view()),
     path('is-authenticated', IsAuthenticated.as_view()),
-    path('current-song', CurrentSong.as_view())
+    path('current-song', CurrentSong.as_view()),
+    path('pause-play-song', ControlSong.as_view()),
 ]
--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -8,7 +8,7 @@ from .util import *
 from api.models import Room
 import os
 
-scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
+scopes = 'app-remote-control streaming user-read-playback-state user-modify-playback-state user-read-currently-playing'
 
 class AuthAURL(APIView):
     def get(self, request, format=None):
@@ -110,3 +110,21 @@ class CurrentSong(APIView):
         }
 
         return Response(song, status=status.HTTP_200_OK)
+    
+class ControlSong(APIView):
+    def put(self, request, format=None):
+        action = self.request.data.get('action')
+        if action not in ('play', 'pause'):
+            return Response({'error': 'Invalid action'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            room = Room.objects.get(code=self.request.session.get('room_code'))
+        except Room.DoesNotExist:
+            return Response({'error': 'Room not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            execute_song_action(room.host, action)
+                
+            return Response({'success': True}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
